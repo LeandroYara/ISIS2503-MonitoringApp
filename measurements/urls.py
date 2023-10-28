4621e3ae211392abb926738df10ea729e0550364
LeandroYara <l.yara@uniandes.edu.co>
2023-10-27 20:39:25 -0500

    Comment on urls file

--- measurements/urls.py
+++ measurements/urls.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
+# The URLs are defined for each method available
 urlpatterns = [
     path('measurements/', views.measurement_list),
     path('measurementcreate/', csrf_exempt(views.measurement_create), name='measurementCreate'),
